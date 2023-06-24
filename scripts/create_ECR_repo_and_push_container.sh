e2ffae017fcb36834bfdbf74ea475b55660f2c9c
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-06-24 19:29:56 -0400

    Changed how ECR repo is created

--- scripts/create_ECR_repo_and_push_container.sh
+++ scripts/create_ECR_repo_and_push_container.sh
@@ -1,16 +1,15 @@
 # Change values below to your own values
 
-# # Create ECR repository
-aws ecr create-repository --repository-name my-repo
+# Prompt user for values
+read -p "Enter your repository name: " my_repo
+read -p "Enter your region: " region
+read -p "Enter your account ID: " account_id
+
+# Create ECR repository
+aws ecr create-repository --repository-name $my_repo
 
 # Authenticate Docker to ECR
-aws ecr get-login-password --region region | docker login --username AWS --password-stdin <account-id>.dkr.ecr.<region>.amazonaws.com
+aws ecr get-login-password --region $region | docker login --username AWS --password-stdin $account_id.dkr.ecr.$region.amazonaws.com
 
 # Build, tag and push image to ECR (note, this is multi-arch because I'm building on my M1 Mac)
-docker buildx build --platform linux/amd64,linux/arm64 --push -t <account-id>.dkr.ecr.<region>.amazonaws.com/chat-aws-slack-bot-dev:latest .
-
-# Tag your image
-docker tag my-image:latest <account-id>.dkr.ecr.<region>.amazonaws.com/my-repo:latest
-
-# Push your image
-docker push <account-id>.dkr.ecr.<region>.amazonaws.com/my-repo:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t $account_id.dkr.ecr.$region.amazonaws.com/$my_repo:latest .
