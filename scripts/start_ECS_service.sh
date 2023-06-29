7e525b72222ee4ced9336409649df16cb96af60c
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-06-29 10:19:46 -0400

    Added bash script to start ECS

---
+++ scripts/start_ECS_service.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+service_arn=$(aws ecs list-services --cluster ChatAWSCluster --query 'serviceArns[0]' --output text)
+aws ecs update-service --cluster ChatAWSCluster --service $service_arn --desired-count 1
