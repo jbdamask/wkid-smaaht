c8aea5342f5131eeaa00516809125b43706d1b3d
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-07-06 09:49:27 -0400

    Clean up

--- src/wkid_smaaht.py
+++ src/wkid_smaaht.py
@@ -39,10 +39,7 @@ def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     say(f"Hey there <@{message['user']}>! :wave:")
 
-# @app.event("message")
-# def handle_message_events(body, logger):
-#     logger.info(body)
-
+# Slack slash command to return list of all available system prompts
 @app.command("/prompts")
 def list_prompts(ack, respond, command):
     ack()
@@ -50,6 +47,7 @@ def list_prompts(ack, respond, command):
     respond(f"{', '.join(p)}")
     # respond(f"{command['text']}")
 
+# Slack slash command to return message associated with a particular system prompt
 @app.command("/get_prompt")
 def show_prompt(ack, respond, command):
     ack()
@@ -58,6 +56,7 @@ def show_prompt(ack, respond, command):
     except Exception as e:
         respond(f"No such system prompt exsists")
 
+# Slack slash command to change system message. This lets users steer Wkid Smaaht at runtime
 @app.command("/set_prompt")
 def set_prompt(ack, respond, command):
     ack()
@@ -78,32 +77,27 @@ def make_image(ack, respond, command):
     else:
         respond(f"{command['text']} caused a problem")
 
-
-# Process DMs
+# Listens for messages and processes if DM
 @app.event("message")
 def handle_message_events(body, context, logger):
     # logger.info(body)
     bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context['bot_user_id']
     channel_id = body['event']['channel']
-    # If the event is from a DM, process it as an app_mention
+    # If the event is from a DM, process as message
     if channel_id.startswith('D'):
-        # your code to handle the message
-        print("We got a DM! Process")
         pass
-    # If it's an app_mention, process the message
+    # If it's an app_mention, return without doing anything
     elif f"<@{bot_user_id}>" in body['event']['text']:
-        # your code to handle the mention
-        print("We got an app mention! Return!")
         return
-    # If it's neither a DM nor an app_mention, return immediately
+    # If it's neither a DM nor an app_mention, return
     else:
         return
-    
-    print("Processing DM message")
+    if DEBUG:
+        print("Processing DM message")
     process_chat(body, context)
 
 
-# @app.event("message")
+# Listens for app mentions and processes (non DM)
 @app.event("app_mention")
 def command_handler(body, context):
     if DEBUG:
@@ -125,16 +119,6 @@ def command_handler(body, context):
         # return immediately if this was a slash command, auto response or image
         return
 
-    # For direct messages, consider all text as the command.
-    # For channel messages, only consider the text after the bot's mention as the command.
-    # if 'channel_type' in body['event'] and body['event']['channel_type'] == 'im':
-    #     command_text = body['event']['text']
-    # else:
-    #     if 'text' not in body['event'] or f"<@{bot_user_id}>" not in body['event']['text']:
-    #         return
-    #     command_text = body['event']['text'].split(f"<@{bot_user_id}>")[1].strip()
-
-    # if body['event']['channel_type'] == 'im':
     # Note: channel_type doesn't appear in JSON objects from Slack threads
     if ('channel_type' in body['event']) and (body['event']['channel_type'] == 'im'):
         command_text = body['event']['text']
@@ -144,51 +128,6 @@ def command_handler(body, context):
         command_text = body['event']['text'].split(f"<@{bot_user_id}>")[1].strip()
 
     process_chat(body, context)
-
-    # if DEBUG:
-    #     print("DEBUG: command_handler - new message")
-    #     print("channel_id: ", channel_id)   
-    #     print("thread_ts: ", thread_ts)   
-    #     print("user_id: ", user_id)
-    #     print("bot_user_id: ", bot_user_id)
-    #     print("command_text: ", command_text)
-    #     print("DEBUG: app.client.chat_postMessage")
-
-    # slack_resp = app.client.chat_postMessage(
-    #     channel=channel_id,
-    #     thread_ts=thread_ts,
-    #     text=WAIT_MESSAGE
-    # )
-    # reply_message_ts = slack_resp['message']['ts']
-    # conversation_history = get_conversation_history(channel_id, thread_ts)
-    # print("got conversation history")
-    # messages = process_conversation_history(conversation_history, bot_user_id, channel_id, thread_ts, user_id)
-    # num_tokens = num_tokens_from_messages(messages)
-
-    # try:
-    #     openai_response = get_completion_from_messages(messages)
-
-    #     if DEBUG:
-    #         print("DEBUG: Got response from OpenAI: ", type(openai_response))
-
-    #     response_text = ""
-    #     ii = 0
-    #     for chunk in openai_response:
-    #         if chunk.choices[0].delta.get('content'):
-    #             ii = ii + 1
-    #             response_text += chunk.choices[0].delta.content
-    #             if ii > N_CHUNKS_TO_CONCAT_BEFORE_UPDATING:
-    #                 update_chat(app, channel_id, reply_message_ts, response_text)
-    #                 ii = 0
-    #         elif chunk.choices[0].finish_reason == 'stop':
-    #             update_chat(app, channel_id, reply_message_ts, response_text)
-    # except Exception as e:
-    #     print(f"Error: {e}")
-    #     app.client.chat_postMessage(
-    #         channel=channel_id,
-    #         thread_ts=thread_ts,
-    #         text=f"I can't provide a response. Encountered an error:\n`\n{e}\n`"
-    #     )
         
     if DEBUG:
         print("DEBUG: end command_handler")
@@ -214,16 +153,6 @@ def process_chat(body, context):
         # return immediately if this was a slash command, auto response or image
         return
 
-    # For direct messages, consider all text as the command.
-    # For channel messages, only consider the text after the bot's mention as the command.
-    # if 'channel_type' in body['event'] and body['event']['channel_type'] == 'im':
-    #     command_text = body['event']['text']
-    # else:
-    #     if 'text' not in body['event'] or f"<@{bot_user_id}>" not in body['event']['text']:
-    #         return
-    #     command_text = body['event']['text'].split(f"<@{bot_user_id}>")[1].strip()
-
-    # if body['event']['channel_type'] == 'im':
     # Note: channel_type doesn't appear in JSON objects from Slack threads
     if ('channel_type' in body['event']) and (body['event']['channel_type'] == 'im'):
         command_text = body['event']['text']
