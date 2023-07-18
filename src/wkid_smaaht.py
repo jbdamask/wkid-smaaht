bee80c2be7939b589fc2e3f0e602a8c07f824f71
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-07-18 10:45:59 -0400

    Making a bit more robust

--- src/wkid_smaaht.py
+++ src/wkid_smaaht.py
@@ -32,14 +32,14 @@ def list_prompts(ack, respond, command):
     ack()
     p = prompt.list_prompts()
     respond(f"{', '.join(p)}")
-    # respond(f"{command['text']}")
+    # respond(f"{command.get(\'text\')}")
 
 # Slack slash command to return message associated with a particular system prompt
 @app.command("/get_prompt")
 def show_prompt(ack, respond, command):
     ack()
     try:
-        respond(f"{prompt.get_prompt(command['text'])}")
+        respond(f"{prompt.get_prompt(command.get('text'))}")
     except Exception as e:
         respond(f"No such system prompt exsists")
 
@@ -47,22 +47,22 @@ def show_prompt(ack, respond, command):
 @app.command("/set_prompt")
 def set_prompt(ack, respond, command):
     ack()
-    logger.info(f"{command['user_id']} : {command['user_name']}")
-    if(prompt.get_prompt(command['text'])) is not None:
-        set_prompt_for_user_and_channel(command['user_id'], command['channel_id'], command['text'])
-        respond(f"Ok, from now on I'll be {command['text']}")
+    logger.info(f"{command.get('user_id')} : {command.get('user_name')}")
+    if(prompt.get_prompt(command.get('text'))) is not None:
+        set_prompt_for_user_and_channel(command.get('user_id'), command.get('channel_id'), command.get('text'))
+        respond(f"Ok, from now on I'll be {command.get('text')}")
     else:
-        respond(f"{command['text']} is not a valid prompt key. Type /prompts to see a list of available system prompts")
+        respond(f"{command.get('text')} is not a valid prompt key. Type /prompts to see a list of available system prompts")
 
 @app.command("/generate_image")
 def make_image(ack, respond, command):
     ack({ "response_type": "in_channel", "text": "Command deprecated. Just type @W'kid Smaaht :pix <your text> instead"})
-    # if(command['text']) is not None:
-    #     logger.info(f"{command['user_id']} : {command['user_name']} : {command['text']}")
-    #     r = generate_image(command['text'])
+    # if(command.get(\'text\')) is not None:
+    #     logger.info(f"{command.get(\'user_id\')} : {command.get(\'user_name\')} : {command.get(\'text\')}")
+    #     r = generate_image(command.get(\'text\'))
     #     respond(r)
     # else:
-    #     respond(f"{command['text']} caused a problem")
+    #     respond(f"{command.get(\'text\')} caused a problem")
 
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
@@ -71,19 +71,24 @@ def make_image(ack, respond, command):
 def message_hello(message, say):
     # say() sends a message to the channel where the event was triggered
     logger.debug("hello command")
-    say(f"Hey there <@{message['user']}>!")
+    say(f"Hey there <@{message.get('user')}>!")
 
 # Process DMs
 @app.event("message")
 def handle_message_events(body, context, logger):
     # logger.info(body)
-    if not is_valid_message_body(body):
+    if not is_valid_message_body(body, context):
         logger.error("Unexpected Slack message body")
         logger.error(body)
+        # TODO: post something about the error to the Slack user
         return
 
-    bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context['bot_user_id']
-    channel_id = body['event']['channel']
+    event = body.get('event')
+    if event is None:
+        return False
+
+    bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context.get('bot_user_id')
+    channel_id = event.get('channel')
     # If the event is from a DM, process it as an app_mention
     if channel_id.startswith('D'):
         # your code to handle the message
@@ -91,7 +96,7 @@ def handle_message_events(body, context, logger):
         pass
     # If it's an app_mention, this will be handled by Slack's @app.event("app_mention") listener. 
     # Return so we don't process twice
-    elif f"<@{bot_user_id}>" in body['event']['text']:
+    elif f"<@{bot_user_id}>" in event.get('text'):
         # your code to handle the mention
         logger.debug("We got an app mention! Return!")
         return
@@ -106,29 +111,48 @@ def handle_message_events(body, context, logger):
 @app.event("app_mention")
 def command_handler(body, context):
     logger.debug(body)
-    if not is_valid_message_body(body):
+    if not is_valid_message_body(body, context):
         logger.error("Unexpected Slack message body")
         logger.error(body)
+        # TODO: post something about the error to the Slack user
         return
     process_event(body, context)
 
-
 # Check it oot, as my Canadian friends say
-def is_valid_message_body(body, bot_user_id):
-    if not ('channel' in body['event']) or not ('text' in body['event']):
+def is_valid_message_body(body, context):
+    # if not ('channel' in body.get('event')) or not ('text' in body.get('event')):
+    #     return False
+    channel_id = None
+    body_text = None
+    channel_type = None
+    event = body.get('event')
+    if event is None:
         return False
-    channel_id = body['event']['channel']
-    body_text = body['event']['text']
 
-    # If we're not a DM and the bot user wasn't called out in the text, something's amiss
-    if not channel_id.startswith('D'):
-        if f"<@{bot_user_id}>" not in body['event']['text']:
-            return False
+    channel_id = event.get('channel')
+    body_text = event.get('text')
 
-    # if not channel_id.startswith('D') and f"<@{bot_user_id}>" not in body['event']['text']:
+    if (channel_id is None) or (body_text is None):
+        return False
+
+    # If DM keep going, otherwise grab bot user id
+    if not event.get('channel_type') or event.get('channel_type') != "im":
+        pass
+    # if ('channel_type' in body.get(\'event\')) and (body.get(\'event\')['channel_type'] == 'im'):
+    #     pass
+    else:
+        bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context.get('bot_user_id')
+        # If we're not a DM and the bot user wasn't called out in the text, but we're here, something's amiss
+        # It seems like this should never happen but I've seen it
+        if not channel_id.startswith('D'):
+            # if f"<@{bot_user_id}>" not in body.get(\'event\')['text']:
+            if f"<@{bot_user_id}>" not in event.get('text'):
+                return False
+
+    # if not channel_id.startswith('D') and f"<@{bot_user_id}>" not in body.get(\'event\')['text']:
     #     return False
 
-    # if (body_text==WAIT_MESSAGE) or (body_text.startswith("/")) or (body_text.startswith("Got your request!")) or (body['event']['blocks'][0]['type'] == "image"):
+    # if (body_text==WAIT_MESSAGE) or (body_text.startswith("/")) or (body_text.startswith("Got your request!")) or (body.get(\'event\')['blocks'][0]['type'] == "image"):
     if (body_text==WAIT_MESSAGE) or (body_text.startswith("/")) or (body_text.startswith("Got your request!")):    
         # return immediately if this was a slash command, auto response 
         return False
@@ -137,12 +161,17 @@ def is_valid_message_body(body, bot_user_id):
 
 # Where's the beef? Oh, it's here
 def extract_command_text(body, context, bot_user_id):
-    if ('channel_type' in body['event']) and (body['event']['channel_type'] == 'im'):
-        command_text = body['event']['text']
+    event = body.get('event')
+    if event is None:
+        return False
+    # if ('channel_type' in body.get('event')) and (body.get('event', {}).get('channel_type') == 'im'):
+    channel_type = event.get('channel_type')
+    if channel_type == 'im':
+        command_text = event.get('text')
     else:
-        if f"<@{bot_user_id}>" not in body['event']['text']:
+        if f"<@{bot_user_id}>" not in event.get('text'):
             return None
-        command_text = body['event']['text'].split(f"<@{bot_user_id}>")[1].strip()
+        command_text = event.get('text').split(f"<@{bot_user_id}>")[1].strip()
 
     return command_text
 
@@ -152,10 +181,14 @@ def process_event(body, context):
     logger.debug(body)
     logger.debug("process_event() context object:)")
     logger.debug(context)
-    bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context['bot_user_id']
-    channel_id = body['event']['channel']
-    thread_ts = body['event'].get('thread_ts', body['event']['ts'])
-    user_id = body['event'].get('user', context.get('user_id'))
+    event = body.get('event')
+    if event is None:
+        return False
+    # THIS ASSUMES THE BODY HAS A PARTICULAR SCHEMA. SOMETHING SHOULD CHECK FIRST
+    bot_user_id = body.get('authorizations')[0]['user_id'] if 'authorizations' in body else context.get('bot_user_id')
+    channel_id = event.get('channel')
+    thread_ts = event.get('thread_ts', event.get('ts'))
+    user_id = event.get('user', context.get('user_id'))
 
     # if not is_valid_message(body, context, bot_user_id):
     #     return
@@ -221,7 +254,8 @@ def process_event(body, context):
         thread_ts=thread_ts,
         text=WAIT_MESSAGE
     )
-    reply_message_ts = slack_resp['message']['ts']
+    # reply_message_ts = slack_resp.get('message')['ts']
+    reply_message_ts = slack_resp.get('message', {}).get('ts')
     conversation_history = get_conversation_history(app, channel_id, thread_ts)
     if conversation_history is None:
         slack_resp = app.client.chat_postMessage(
