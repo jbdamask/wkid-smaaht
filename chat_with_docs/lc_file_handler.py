a5257860c1e14577df8934c364ebfbfd8deaefe0
@jbdamaskfsp <jdamask@flagshippioneering.com>
2023-10-20 08:16:09 -0400

    First pass with pattern of transforming PDF to HTML, then chopping up a document with section information retained in metadata tags. This doesn't work as-is, though because the text includes chuunky HTML which quickly hits API limit when calling OpenAI embeddings.

--- chat_with_docs/lc_file_handler.py
+++ chat_with_docs/lc_file_handler.py
@@ -132,6 +132,74 @@ class PDFHandlerStructured(Handler):
     def instantiate_loader(self, filename):
         self.loader = PDFMinerPDFasHTMLLoader(filename)
 
+    def load_and_split(self):
+        self.instantiate_loader(self.filepath)
+        data = self.loader.load()[0]
+        from bs4 import BeautifulSoup
+        soup = BeautifulSoup(data.page_content,'html.parser')
+        content = soup.find_all('div')     
+        
+        # documents = self.loader.load()
+        import re
+        cur_fs = None
+        cur_text = ''
+        snippets = []   # first collect all snippets that have the same font size
+        for c in content:
+            sp = c.find('span')
+            if not sp:
+                continue
+            st = sp.get('style')
+            if not st:
+                continue
+            fs = re.findall('font-size:(\d+)px',st)
+            if not fs:
+                continue
+            fs = int(fs[0])
+            if not cur_fs:
+                cur_fs = fs
+            if fs == cur_fs:
+                cur_text += c.text
+            else:
+                snippets.append((cur_text,cur_fs))
+                cur_fs = fs
+                cur_text = c.text
+        snippets.append((cur_text,cur_fs))
+
+        from langchain.docstore.document import Document
+        cur_idx = -1
+        semantic_snippets = []
+        # Assumption: headings have higher font size than their respective content
+        for s in snippets:
+            # if current snippet's font size > previous section's heading => it is a new heading
+            if not semantic_snippets or s[1] > semantic_snippets[cur_idx].metadata['heading_font']:
+                metadata={'heading':s[0], 'content_font': 0, 'heading_font': s[1]}
+                metadata.update(data.metadata)
+                semantic_snippets.append(Document(page_content='',metadata=metadata))
+                cur_idx += 1
+                continue
+
+            # if current snippet's font size <= previous section's content => content belongs to the same section (one can also create
+            # a tree like structure for sub sections if needed but that may require some more thinking and may be data specific)
+            if not semantic_snippets[cur_idx].metadata['content_font'] or s[1] <= semantic_snippets[cur_idx].metadata['content_font']:
+                semantic_snippets[cur_idx].page_content += s[0]
+                semantic_snippets[cur_idx].metadata['content_font'] = max(s[1], semantic_snippets[cur_idx].metadata['content_font'])
+                continue
+
+            # if current snippet's font size > previous section's content but less than previous section's heading than also make a new
+            # section (e.g. title of a PDF will have the highest font size but we don't want it to subsume all sections)
+            metadata={'heading':s[0], 'content_font': 0, 'heading_font': s[1]}
+            metadata.update(data.metadata)
+            semantic_snippets.append(Document(page_content='',metadata=metadata))
+            cur_idx += 1
+
+        # self.docs = self.text_splitter.split_documents(documents)
+        self.docs = self.text_splitter.split_documents(semantic_snippets)
+        filename = self.file.get('name')
+        for idx, text in enumerate(self.docs):
+            self.docs[idx].metadata['filename'] = filename.split('/')[-1]   
+        self.filtered_docs = utils.filter_complex_metadata(self.docs)
+      
+
 class DOCXHandler(Handler):
     def handle(self):
         return f"Handling DOCX file: {self.file}"
@@ -268,7 +336,8 @@ class HandlerFactory:
     # filetype. Instead, we instantiate it directly if we know we're
     # dealing with a web page
     handlers = {
-        "pdf": PDFHandler, 
+        # "pdf": PDFHandler, 
+        "pdf": PDFHandlerStructured,
         "docx": DOCXHandler, 
         "txt": TxtHandler,
         # "xlsx": ExcelHandler, 
